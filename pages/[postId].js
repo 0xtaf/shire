9852e97b764636c82552f0201ddff28174c705ec
Tayfun Sur <tayfunsur@gmail.com>
2020-04-07 09:05:50 +0300

    styling is fixed to start improving

--- pages/[postId].js
+++ pages/[postId].js
@@ -29,52 +29,7 @@ const Home = () => (
           <a className="blog-title-link">iyi bir bloğun 2983 özelliği</a>
         </Link>
       </h2>
-      <div>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Eget mi proin sed
-        libero enim. Ultrices tincidunt arcu non sodales. Amet est placerat in
-        egestas erat imperdiet sed euismod nisi. Morbi leo urna molestie at
-        elementum eu. Suspendisse ultrices gravida dictum fusce ut. Nullam non
-        nisi est sit amet. Eget mauris pharetra et ultrices. Auctor urna nunc id
-        cursus metus aliquam eleifend. Quam nulla porttitor massa id neque
-        aliquam vestibulum morbi blandit. Scelerisque mauris pellentesque
-        pulvinar pellentesque habitant morbi tristique senectus. Sit amet dictum
-        sit amet justo donec enim. Augue eget arcu dictum varius duis. Hendrerit
-        dolor magna eget est lorem ipsum. Nec dui nunc mattis enim ut tellus
-        elementum sagittis.
-      </div>
-      <div className="blog-date">7 Nisan 2020 </div>
-    </div>
-
-    <div className="blog">
-      <h2 className="blog-title">
-        <Link href="/test">
-          <a className="blog-title-link">iyi bir bloğun 2983 özelliği</a>
-        </Link>
-      </h2>
-      <div>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Eget mi proin sed
-        libero enim. Ultrices tincidunt arcu non sodales. Amet est placerat in
-        egestas erat imperdiet sed euismod nisi. Morbi leo urna molestie at
-        elementum eu. Suspendisse ultrices gravida dictum fusce ut. Nullam non
-        nisi est sit amet. Eget mauris pharetra et ultrices. Auctor urna nunc id
-        cursus metus aliquam eleifend. Quam nulla porttitor massa id neque
-        aliquam vestibulum morbi blandit. Scelerisque mauris pellentesque
-        pulvinar pellentesque habitant morbi tristique senectus. Sit amet dictum
-        sit amet justo donec enim. Augue eget arcu dictum varius duis. Hendrerit
-        dolor magna eget est lorem ipsum. Nec dui nunc mattis enim ut tellus
-        elementum sagittis.
-      </div>
-      <div className="blog-date">7 Nisan 2020 </div>
-    </div>
-    <div className="blog">
-      <h2 className="blog-title">
-        <Link href="/test">
-          <a className="blog-title-link">iyi bir bloğun 2983 özelliği</a>
-        </Link>
-      </h2>
-      <div>
+      <div className="blog-text">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Eget mi proin sed
         libero enim. Ultrices tincidunt arcu non sodales. Amet est placerat in
