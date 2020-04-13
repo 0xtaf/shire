16a6f222e165202681d349821868e5aff4facfd2
Tayfun Sur <tayfunsur@gmail.com>
2020-04-13 14:29:12 +0300

    basic blog form is created

--- pages/add.js
+++ pages/add.js
@@ -3,15 +3,26 @@ import React from 'react';
 const Form = () => {
   return (
     <div>
-      <form action="/add" method='POST'>
-        <label for="title">title</label>
-        <input type="textarea" name="title"></input>
-        <label for="title">details</label>
-        <input type="textarea" name="details"></input>
-        <button type="submit"></button>
+      <form action="" method="POST">
+        <div>
+          <label for="title">Title</label>
+          <input id="title" type="text" name="title"/>
+        </div>
+        <div>
+          <label for="details">Blogpost</label>
+          <textarea id="details" cols="" rows="10" name="details"/>
+        </div>
+
+        <button type="submit">Send</button>
       </form>
-    </div>  
-  )
-}
+
+      <style jsx>{`
+        label {
+          display: block;
+        }
+      `}</style>
+    </div>
+  );
+};
 
 export default Form;
