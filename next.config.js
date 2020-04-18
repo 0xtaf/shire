1814f5f70ee868da364cbb534622c27da77ea227
Tayfun Sur <tayfunsur@gmail.com>
2020-04-18 11:11:43 +0300

    css for ssr config is added

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
+const withCSS = require('@zeit/next-css');
+
 module.exports = {
+  cssModules: true,
   env: {
-    MONGO_URI: "mongodb+srv://taf:268451881@cluster0-a420i.mongodb.net/blog?retryWrites=true&w=majority"
-  }
-}
+    MONGO_URI:
+      'mongodb+srv://taf:268451881@cluster0-a420i.mongodb.net/blog?retryWrites=true&w=majority',
+  },
+};
