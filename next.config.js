5d6632d8dfa5a6b5fc50292ce1e10a86fd1d2cfc
Tayfun Sur <tayfunsur@gmail.com>
2020-04-19 20:31:47 +0300

    mongo configurations changed

--- next.config.js
+++ next.config.js
@@ -2,12 +2,7 @@ require('dotenv').config();
 
 module.exports = {
   env: {
-    SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
-  },
-};
-
-module.exports = {
-  env: {
     MONGO_URI: process.env.MONGO_URI,
+    SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
   },
 };
