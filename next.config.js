194d535332f6a14e801855362c9919dbc85cf455
Tayfun Sur <tayfunsur@gmail.com>
2020-04-19 17:52:13 +0300

    localhost is set again

--- next.config.js
+++ next.config.js
@@ -1,17 +1,13 @@
-require('dotenv').config()
+require('dotenv').config();
 
 module.exports = {
   env: {
-    SENDGRID_API_KEY: process.env.SENDGRID_API_KEY
-  }
-}
-
-
-
-module.exports = {
-  env: {
-    MONGO_URI:
-      'mongodb+srv://taf:268451881@cluster0-a420i.mongodb.net/blog?retryWrites=true&w=majority',
+    SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
   },
 };
 
+module.exports = {
+  env: {
+    MONGO_URI: process.env.MONGO_URI,
+  },
+};
