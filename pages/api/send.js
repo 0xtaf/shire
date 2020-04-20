f52b05947b5c6cfd3e8d1e0c54de0da76442fe53
Tayfun Sur <tayfunsur@gmail.com>
2020-04-20 13:08:13 +0300

    name function is being fetched

--- pages/api/send.js
+++ pages/api/send.js
@@ -3,12 +3,12 @@ const sgMail = require('@sendgrid/mail')
 export default async function(req, res) {
   sgMail.setApiKey(process.env.SENDGRID_API_KEY)
   console.log(process.env.SENDGRID_API_KEY)
-  const { name, email, message } = req.body
+  const { senderName, email, message } = req.body
   console.log(req.body)
   const content = {
     to: 'tayfunsur@gmail.com',
     from: 'tayfunsur@gmail.com',
-    subject: `New Message From - ${name} / ${email}`,
+    subject: `New Message From - ${senderName} / ${email}`,
     text: message,
     html: `<p>${message}</p>`
   }
