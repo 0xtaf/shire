1b49cf884ff631eb629a39b829148c08837c8237
Tayfun Sur <tayfunsur@gmail.com>
2020-04-20 13:04:43 +0300

    sending mail function is added

--- pages/api/send.js
+++ pages/api/send.js
@@ -3,12 +3,12 @@ const sgMail = require('@sendgrid/mail')
 export default async function(req, res) {
   sgMail.setApiKey(process.env.SENDGRID_API_KEY)
   console.log(process.env.SENDGRID_API_KEY)
-  const { email, message } = req.body
+  const { name, email, message } = req.body
   console.log(req.body)
   const content = {
     to: 'tayfunsur@gmail.com',
-    from: email,
-    subject: `New Message From - ${email}`,
+    from: 'tayfunsur@gmail.com',
+    subject: `New Message From - ${name} / ${email}`,
     text: message,
     html: `<p>${message}</p>`
   }
