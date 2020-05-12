717f214fc6ff1520d3ff28fd66e3fe4ffd50db19
Tayfun Sur <tayfunsur@gmail.com>
2020-05-12 09:02:50 +0300

    auth context was created and main app is wrapped within auth provider

---
+++ Context/AuthContext.js
@@ -0,0 +1,29 @@
+import React, {createContext,useState,useEffect} from 'react';
+import AuthService from '../Services/AuthService';
+
+//by using createContext we create a provider and a consumer 
+//bunu export ediyoruz yani addposttakine. komple fonksiyonu değil
+export const AuthContext = createContext();
+
+export default ({ children })=>{
+    const [user,setUser] = useState(null);
+    const [isAuthenticated,setIsAuthenticated] = useState(false);
+    const [isLoaded,setIsLoaded] = useState(false);
+
+    useEffect(()=>{
+        AuthService.isAuthenticated().then(data =>{
+            setUser(data.user);
+            setIsAuthenticated(data.isAuthenticated);
+            setIsLoaded(true);
+        });
+    },[]);
+
+    return (
+        <div>
+            {!isLoaded ? <h1>Loading</h1> : 
+            <AuthContext.Provider value={{user,setUser,isAuthenticated,setIsAuthenticated}}>
+                { children }
+            </AuthContext.Provider>}
+        </div>
+    )
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
 import 'normalize.css';
-import '../styles/app.css'
-
+import '../styles/app.css';
+import AuthProvider from '../Context/AuthContext';
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  );
 }
