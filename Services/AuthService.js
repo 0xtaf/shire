17514756beb211ed6d55a29bbb29f49db1399ffb
Tayfun Sur <tayfunsur@gmail.com>
2020-05-12 08:05:00 +0300

    auth service is created

---
+++ Services/AuthService.js
@@ -0,0 +1,38 @@
+export default {
+  login: (user) => {
+    console.log(user);
+    return fetch('/api/panel-login', {
+      method: 'post',
+      body: JSON.stringify(user),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }).then((res) => {
+      if (res.status !== 401) return res.json().then((data) => data);
+      else return { isAuthenticated: false, user: { username: '', role: '' } };
+    });
+  },
+  register: (user) => {
+    console.log(user);
+    return fetch('/api/signup', {
+      method: 'post',
+      body: JSON.stringify(user),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => data);
+  },
+  logout: () => {
+    return fetch('/api/logout')
+      .then((res) => res.json())
+      .then((data) => data);
+  },
+  isAuthenticated: () => {
+    return fetch('/api/authenticated').then((res) => {
+      if (res.status !== 401) return res.json().then((data) => data);
+      else return { isAuthenticated: false, user: { username: '', role: '' } };
+    });
+  },
+};
