f65ed99a4b6e8d1d9061a10dd4932d2e84e6bb01
Tayfun Sur <tayfunsur@gmail.com>
2020-05-13 11:30:51 +0300

    authcontext and authservice bugs are fixed

--- Services/AuthService.js
+++ Services/AuthService.js
@@ -1,39 +1,43 @@
 export default {
-  login: (user) => {
-    console.log(user);
-    return fetch('/api/panel-login', {
-      method: 'post',
-      body: JSON.stringify(user),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    }).then((res) => {
-      if (res.status !== 401) return res.json().then((data) => data);
-      else return { isAuthenticated: false, user: { username: '', role: '' } };
-    });
+  login : user =>{
+      console.log(user);
+      return fetch('/api/panel-login',{
+          method : "post",
+          body : JSON.stringify(user),
+          headers : {
+              'Content-Type' : 'application/json'
+          }
+      }).then(res => {
+          if(res.status !== 401)
+              return res.json().then(data => data);
+          else
+              return { isAuthenticated : false, user : {username : "",isAdmin : ""}};
+      })
   },
-  register: (user) => {
-    console.log(user);
-    return fetch('/api/signup', {
-      method: 'post',
-      body: JSON.stringify(user),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-      .then((res) => res.json())
-      .then((data) => data);
+  register : user =>{
+      console.log(user);
+      return fetch('/api/userCreate',{
+          method : "post",
+          body : JSON.stringify(user),
+          headers : {
+              'Content-Type' : 'application/json'
+          }
+      }).then(res => res.json())
+        .then(data => data);
   },
-  logout: () => {
-    return fetch('/api/logout')
-      .then((res) => res.json())
-      .then((data) => data);
+  logout : ()=>{
+      return fetch('/api/logout')
+              .then(res => res.json())
+              .then(data => data);
   },
-  isAuthenticated: () => {
-    console.log("isauth")
-    return fetch('/api/authenticated').then((res) => {
-      if (res.status !== 401) return res.json().then((data) => data);
-      else return { isAuthenticated: false, user: { username: '', isAdmin: '' } };
-    });
-  },
-};
+  isAuthenticated : ()=>{
+      return fetch('/api/authenticated')
+              .then(res=>{
+                  if(res.status !== 401)
+                      return res.json().then(data => data);
+                  else
+                      return { isAuthenticated : false, user : {username : "",isAdmin : ""}};
+              });
+  }
+
+}
