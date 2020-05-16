827ff9dcf1076a03e64f3847652cf01a940274d0
Tayfun Sur <tayfunsur@gmail.com>
2020-05-16 13:44:07 +0300

    send api get method got prohibited

--- pages/api/send.js
+++ pages/api/send.js
@@ -1,6 +1,9 @@
 const sgMail = require('@sendgrid/mail');
 
 export default async (req, res) => {
+  const {method} = req;
+  console.log(method)
+
   sgMail.setApiKey(process.env.SENDGRID_API_KEY);
   const { senderName, email, message } = req.body;
   const content = {
@@ -11,10 +14,15 @@ export default async (req, res) => {
     html: `<p>${message}</p>`,
   };
 
-  try {
-    await sgMail.send(content);
-    res.status(200).send('Message sent successfully.');
-  } catch (error) {
-    res.status(400).send('Message not sent.');
+  if (method === 'POST'){
+    try {
+      await sgMail.send(content);
+      res.status(200).send('Message sent successfully.');
+    } catch (error) {
+      res.status(400).send('Message not sent.');
+    }
+  } else {
+    res.status(400).send('This action is not allowed');
   }
+
 };
